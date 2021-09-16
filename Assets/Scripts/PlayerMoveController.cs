ac0d94bee5570457a8c6a0d9e8f5e42f99f5907e
Wilder Herrera <wilder.herrera@tekus.co>
2021-09-16 18:43:19 -0500

    agrega contador en la pantalla y se crea lógica para aumentar contador al colisionar con los elementos

--- Assets/Scripts/PlayerMoveController.cs
+++ Assets/Scripts/PlayerMoveController.cs
@@ -1,27 +1,36 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PlayerMoveController : MonoBehaviour
 {
-    private readonly float _speed = 40.0f;
-    private readonly float _turnSpeed = 24.0f;
-    private float _horizontalInput;
-    private float _forwardInput;
+    private readonly float speed = 40.0f;
+    private readonly float turnSpeed = 24.0f;
+    private float horizontalInput;
+    private float forwardInput;
+    private int pointsCount = 0;
+    public Text pointsText;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        _horizontalInput = Input.GetAxis("Horizontal");
-        _forwardInput = Input.GetAxis("Vertical");
+        horizontalInput = Input.GetAxis("Horizontal");
+        forwardInput = Input.GetAxis("Vertical");
 
-        transform.Translate(_forwardInput * _speed * Time.deltaTime * Vector3.forward);
-        transform.Rotate(_horizontalInput * _turnSpeed * Time.deltaTime * Vector3.up);
+        transform.Translate(forwardInput * speed * Time.deltaTime * Vector3.forward);
+        transform.Rotate(horizontalInput * turnSpeed * Time.deltaTime * Vector3.up);
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        pointsText.text = $"Puntuación: { pointsCount += 1 }";
+        Debug.Log("puntos" + pointsCount);
     }
 }
